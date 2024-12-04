da9a7fc80eba89fe4ce6a8bc004b07dd2df9017f
ZP0505 <116180810+ZP0505@users.noreply.github.com>
2024-12-05 06:22:48 +0800

    Update Game.py

--- Game.py
+++ Game.py
@@ -14,6 +14,9 @@ import threading
 from datetime import datetime
 from colorama import init, Fore, Style
 from version_updater import UpdateManager
+import win32gui
+import win32con
+import win32api
 
 def run_update_and_restart():
     update_manager = UpdateManager(
@@ -24,6 +27,10 @@ def run_update_and_restart():
     update_manager.run_update()
 
 run_update_and_restart()
+
+def get_detections(image):
+    results = model(image)
+    return results[0]
 
 init(autoreset=True)
 
@@ -58,53 +65,16 @@ def setup_colored_logging():
     logger.addHandler(console_handler)
     return logger
 
-
 logger = setup_colored_logging()
 
 model = YOLO("best.pt")
 
-def get_detections(image):
-    results = model(image)
-    return results[0]
+def get_window_handle(title_pattern="Game - Crystal Caves - Google Chrome"):
+    hwnd = win32gui.FindWindow(None, title_pattern)
+    if hwnd:
+        return hwnd
 
-def Get_FeeGas():
-    url = "https://base.blockpi.network/v1/rpc/public"
-    payload = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_feeHistory\",\"params\":[\"0xa\",\"latest\",[25,75]]}"
-    headers = {
-        'Accept': "application/json, text/plain, */*",
-        'Sec-Fetch-Site': "cross-site",
-        'Origin': "https://ct.app",
-        'Sec-Fetch-Mode': "cors",
-        'sec-ch-ua': "\"Not)A;Brand\";v=\"99\", \"Google Chrome\";v=\"127\", \"Chromium\";v=\"127\"",
-        'sec-ch-ua-mobile': "?0",
-        'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
-        'Content-Type': "application/json",
-        'Accept-Encoding': "gzip, deflate, br, zstd",
-        'Host': "base.blockpi.network",
-        'Referer': "https://ct.app/",
-        'Connection': "keep-alive",
-        'sec-ch-ua-platform': "\"Windows\"",
-        'Accept-Language': "zh-CN,zh;q=0.9",
-        'Sec-Fetch-Dest': "empty",
-    }
-    response = requests.request("POST", url, data=payload, headers=headers)
-    data = response.json()
-    baseFeePerGas = data["result"]["baseFeePerGas"]
-    base_fee_per_gas = [int(value, 16) for value in baseFeePerGas]
-    max_value = max(base_fee_per_gas)
-    gas = max_value/100000000+0.01
-    gas = round(gas, 3)
-    return gas
-
-def monitor_gas():
-    while True:
-        gas = Get_FeeGas()
-        logger.info(f"当前Gas值: {gas}")
-        if gas <= 0.35:
-            break
-        time.sleep(5)
-
-def get_browser_window(title="Game - Crystal Caves"):
+def get_browser_window(title="Game - Crystal Caves - Google Chrome"):
     windows = gw.getWindowsWithTitle(title)
     if len(windows) > 0:
         return windows[0]
@@ -140,52 +110,82 @@ def logstr_detections(results):
                 closest_block = block
     return person_pos, closest_block
 
-def find_and_double_click(results, img, browser_window, person_pos=None):
+def background_double_click(hwnd, x, y):
+    client_x, client_y = win32gui.ScreenToClient(hwnd, (int(x), int(y)))
+    lParam = win32api.MAKELONG(client_x, client_y)
+    win32gui.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0, lParam)
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+    time.sleep(0.05)
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, lParam)
+    time.sleep(0.1)
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+    time.sleep(0.05)
+    win32gui.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, lParam)
+
+def find_and_double_click(results, img, browser_window, window_x, window_y, hwnd, person_pos=None):
     person_pos, closest_block = logstr_detections(results)
     if closest_block is not None:
         block_x, block_y, block_id = closest_block
-        click_position = (block_x, block_y)
-        pyautogui.doubleClick(click_position[0], click_position[1])
-        return True
+        click_x = window_x + block_x
+        click_y = window_y + block_y
+        if hwnd:
+            background_double_click(hwnd, click_x, click_y)
+            return True
+        else:
+            logger.warning("无法找到窗口句柄")
+            return False
     else:
         logger.warning("没有找到最近的方块，跳过双击操作")
         return False
 
-def find_and_click_image(image_path):
+def background_click_image(image_path, hwnd):
     try:
         location = pyautogui.locateOnScreen(image_path, confidence=0.8)
         if location:
             center = pyautogui.center(location)
-            pyautogui.click(center)
+            client_x, client_y = win32gui.ScreenToClient(hwnd, center)
+            lParam = win32api.MAKELONG(client_x, client_y)
+            win32gui.SendMessage(hwnd, win32con.WM_MOUSEMOVE, 0, lParam)
+            win32gui.SendMessage(hwnd, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, lParam)
+            time.sleep(0.05)
+            win32gui.SendMessage(hwnd, win32con.WM_LBUTTONUP, 0, lParam)
             return center
         else:
             return None
     except pyautogui.ImageNotFoundException:
-        logger.warning("未找到图像。")
+        logger.warning(f"未找到图像: {image_path}")
+        return None
 
-def handle_post_double_click():
-    template_path = 'confirm.png'
-    image_location = find_and_click_image("confirm.png")
-    for i in range(10):
-        image_location_qr = find_and_click_image("qrjy.png")
-        if image_location_qr:
-            break
+def handle_post_double_click(hwnd):
+    background_click_image("confirm.png", hwnd)
+    for i in range(5):
+        hwnd1=get_window_handle(title_pattern="OKX Wallet")
+        if hwnd1:
+            if background_click_image("qrjy.png", hwnd1):
+                break
         time.sleep(1)
 
 def main():
     logger.info("游戏机器人启动")
-    browser_window = get_browser_window("Game - Crystal Caves")
-    logger.info(f"已找到游戏窗口")
-    if browser_window is None:
+    hwnd = get_window_handle()
+    browser_window = get_browser_window()
+    if not hwnd or not browser_window:
+        logger.error("无法找到游戏窗口")
         return
+    logger.info(f"已找到游戏窗口，句柄: {hwnd}")
     person_pos = None
     counter = 0
     while True:
-        monitor_gas()
-        image_location_ok = find_and_click_image("ok.png")
+        try:
+            monitor_gas()
+        except Exception as e:
+            logger.error(f"Gas监控出错: {e}")
+            time.sleep(5)
+            continue
+        background_click_image("ok.png", hwnd)
         if counter % 5 == 0:
-            find_and_click_image("dw.png")
-            find_and_click_image("dw2.png")
+            background_click_image("dw.png", hwnd)
+            background_click_image("dw2.png", hwnd)
         img, window_x, window_y = capture_browser_window(browser_window)
         results = get_detections(img)
         person_pos, closest_block = logstr_detections(results)
@@ -194,17 +194,43 @@ def main():
         if closest_block:
             logger.info(f"离人物最近的方块ID: {closest_block[2]}, 坐标: {closest_block[0]}, {closest_block[1]}")
         if closest_block:
-            find_and_double_click(results, img, browser_window, person_pos)
+            find_and_double_click(results, img, browser_window, window_x, window_y, hwnd, person_pos)
         else:
             logger.warning("未找到最近的方块")
-            find_and_click_image("dw.png")
-            find_and_click_image("dw2.png")
-            handle_post_double_click()
+            background_click_image("dw.png", hwnd)
+            background_click_image("dw2.png", hwnd)
+            handle_post_double_click(hwnd)
         counter += 1
         time.sleep(3)
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
         time.sleep(1)
+
+def Get_FeeGas():
+    url = "https://base.blockpi.network/v1/rpc/public"
+    payload = "{\"jsonrpc\":\"2.0\",\"id\":1,\"method\":\"eth_feeHistory\",\"params\":[\"0xa\",\"latest\",[25,75]]}"
+    headers = {
+        'Accept': "application/json, text/plain, */*",
+        'Sec-Fetch-Site': "cross-site",
+        'Origin': "https://ct.app",
+        'Sec-Fetch-Mode': "cors",
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    data = response.json()
+    baseFeePerGas = data["result"]["baseFeePerGas"]
+    base_fee_per_gas = [int(value, 16) for value in baseFeePerGas]
+    max_value = max(base_fee_per_gas)
+    gas = max_value/100000000+0.01
+    gas = round(gas, 3)
+    return gas
+
+def monitor_gas():
+    while True:
+        gas = Get_FeeGas()
+        logger.info(f"当前Gas值: {gas}")
+        if gas <= 0.35:
+            break
+        time.sleep(5)
 
 if __name__ == "__main__":
     main()
